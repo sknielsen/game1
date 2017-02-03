3b2b5b5fd715dc9401dbea28fce0edcd4fe65ba6
Samantha Nielsen <samanthan@concur.com>
2017-02-03 11:53:01 -0800

    Update Die class to store die images in dictionary
    -Update turn function to use while loop
    -Add begin game funtion to prompt for number of players and store as
    list

--- farkle.py
+++ farkle.py
@@ -5,20 +5,55 @@ class Die(object):
     def __init__(self, number, sides=6):
         self.number = number
         self.sides = sides
+        self._value_to_str = { 1: """
+            ________            
+           /\       \           
+          /  \   ()  \          
+     %s.  /    \_______\         
+         \    /       /         
+          \  /       /          
+           \/_______/           """, 2: """
+            ________            
+           /\ ()    \           
+          /  \     ()\          
+     %s.  /    \_______\         
+         \    /       /         
+          \  /       /          
+           \/_______/           """, 3: """
+            ________            
+           /\ ()    \           
+          /  \   ()  \          
+     %s.  /    \_____()\         
+         \    /       /         
+          \  /       /          
+           \/_______/           """, 4: """
+            ________            
+           /\ () () \           
+          /  \       \          
+     %s.  /    \_()_()_\         
+         \    /       /         
+          \  /       /          
+           \/_______/           """, 5: """
+            ________            
+           /\ () () \           
+          /  \   ()  \          
+     %s.  /    \_()_()_\         
+         \    /       /         
+          \  /       /          
+           \/_______/           """, 6: """
+            ________            
+           /\ () () \           
+          /  \ () () \          
+     %s.  /    \_()_()_\         
+         \    /       /         
+          \  /       /          
+           \/_______/           """}
 
     def roll(self):
-        self.die_value = random.randint(1, self.sides)
+        self.value = random.randint(1, self.sides)
 
     def __str__(self):
-        return("""
-                  ________            
-                 /\       \           
-                /  \   %i   \          
-         %i.    /    \_______\         
-               \    /       /         
-                \  /       /          
-                 \/_______/           """) % (self.die_value, self.number)
-
+        return self._value_to_str[self.value] % self.number
 
 die1 = Die(1)
 die2 = Die(2)
@@ -26,6 +61,11 @@ die3 = Die(3)
 die4 = Die(4)
 die5 = Die(5)
 die6 = Die(6)
+
+def begin_game():
+    players = int(raw_input('Hello and welcome to Farkle! How many players are there? '))
+    players = range(1, (players + 1))
+    return players
 
 def begin_turn():
     die1.roll()
@@ -41,21 +81,23 @@ def begin_turn():
     die6.roll()
     print(die6)
 
-def player_turn(num_of_players):
-    players = 1
-    while players <= num_of_players:
-        print("It is player number %s's turn!") % (players)
-        begin_turn()
-        kept_dice = raw_input("Which number dice would you like to keep? (ie '1 3 4' or 'none') ")
-        if kept_dice.lower() == 'none':
-            print('FARKLE!')
-        else:
-            kept_dice = map(int, kept_dice.split())
-        players += 1
+def player_turn(players):
+    still_playing = True
+    while still_playing == True:
+        for player in players:
+            print("It is player number %i's turn!") % (player)
+            begin_turn()
+            kept_dice = raw_input("Which number dice would you like to keep? (ie '1 3 4' or 'none') ")
+            if kept_dice.lower() == 'none':
+                print('FARKLE!')
+            else:
+                kept_dice = map(int, kept_dice.split())
+
+
+
 
 def main():
-    num_of_players = int(raw_input('Hello and welcome to Farkle! How many players are there? '))
-    player_turn(num_of_players)
+    player_turn(begin_game())
 
 if __name__ == '__main__':
     main()
