05185643ba5dd64ea824293d03a80590ee447f3f
Samantha Nielsen <samanthan@concur.com>
2017-03-02 17:47:20 -0800

    Update scoring tools

--- scoring_tools.py
+++ scoring_tools.py
@@ -3,56 +3,48 @@ import random
 dice_values = []
 for number in range(6):
     dice_values.append(random.randint(1, 7))
-print(dice_values)
 
-def n_of_a_kind(n):
+def create_value_count_dict(dice_values):
     value_counts = {}
     for die in dice_values:
         if die in value_counts:
             value_counts[die] += 1
         else:
             value_counts[die] = 1
+    return value_counts
+
+def n_of_a_kind(n, value_counts):
     for value in value_counts:
-        if value == n
-        return True
+        if value_counts[value] == n:
+            return True
 
-# Check for six of a kind
-if n_of_a_kind(6):
-    score = 3000
+def score_of_1s(value_counts):
+    if value_counts[1] == 1 or value_counts[1] == 2:
+        if value_count[1] == 1:
+            score = 100
+        else:
+            score = 200
+    else:
+        score = 0
     return score
 
-# Check for straight
-if sort(dice_values( == [1, 2, 3, 4, 5, 6]
-    score = 1500
+def score_of_5s(value_counts):
+    if value_counts[5] == 1 or value_counts[5] == 2:
+        if value_count[5] == 1:
+            score = 50
+        else:
+            score = 100
+    else:
+        score = 0
     return score
 
-# Check for five of a kind
-if n_of_a_kind(5):
-    score = 2000
-    return score
-
-def four_of_a_kind(dice):
-# Check for four of a kind or four and pair
-    four_of_kind = False
-    pair = False
-    if n_of_a_kind(4):
-        four_of_kind = True
-    if n_of_a_kind(2):
-        pair = True
-    if four_of_kind and pair:
-        score = 1500
-        return score
-    elif four_of_kind:
-        score = 1000
-        return score
-
-def three_of_a_kind(dice):
+def three_of_a_kind(value_counts):
 # Check for three of a kinds
     three_of_a_kind_values = []
     for value in value_counts:
         if value_counts[value] == 3:
-        three_of_a_kind_values.append[value]
-    if len(num_of_three_kinds) == 2:
+            three_of_a_kind_values.append(value)
+    if len(three_of_a_kind_values) == 2:
         score = 2500
         return score
     else:
@@ -71,7 +63,54 @@ def three_of_a_kind(dice):
         elif three_of_a_kind_values== [5]:
             score = 400
             return score
-        else three_of_a_kind_values== [6]:
+        elif three_of_a_kind_values== [6]:
             score = 300
             return score
+
+def four_of_a_kind(value_counts):
+# Check for four of a kind or four and pair
+    four_of_kind = False
+    pair = False
+    if n_of_a_kind(4, value_counts):
+        four_of_kind = True
+    if n_of_a_kind(2, value_counts):
+        pair = True
+    if four_of_kind and pair:
+        score = 1500
+        return score
+    elif four_of_kind:
+        score = 1000
+        return score
+
+def calculate_score(dice_values):
+    value_counts = create_value_count_dict(dice_values)
+    # Check for six of a kind
+    if n_of_a_kind(6, value_counts):
+        score = 3000
+        return score
+    # Check for straight
+    elif sorted(dice_values) == [1, 2, 3, 4, 5, 6]:
+        score = 1500
+        return score
+    # Check for five of a kind
+    elif n_of_a_kind(5, value_counts):
+        score = 2000
+        score += score_of_1s() + score_of_5s()
+        return score
+    elif four_of_a_kind(value_counts):
+        score = four_of_a_kind(value_counts)
+        if score == 1000:
+            score += score_of_1s() + score_of_5s()
+        return score
+    elif three_of_a_kind(value_counts):
+        score = three_of_a_kind(value_counts)
+        if score != 2500:
+            score += score_of_1s(value_counts) + score_of_5s(value_counts)
+        return score
+    else:
+        score = score_of_1s(value_counts) + score_of_5s(value_counts)
+        return score
+
+print dice_values
+print calculate_score(dice_values)
         
